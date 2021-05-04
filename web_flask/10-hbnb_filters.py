16fae0fc76351797e18e25f6e7dcc103bef2972c
brerickner <breannarickner@gmail.com>
2021-05-04 02:16:24 +0000

    10-hbnb_filters.py

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''Module to start Flask web app'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
+from sqlalchemy import orm
+
+app = Flask(__name__)
+HOST = '0.0.0.0'
+PORT = 5000
+
+
+@app.teardown_appcontext
+def teardown(context):
+    '''Method to remove current SQLAlchemy Session'''
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    '''Method to load classes'''
+    stateDict = storage.all(State)
+    amenDict = storage.all(Amenity)
+
+    return render_template('10-hbnb_filters.html',
+                           stateDict=stateDict, amenDict=amenDict)
+
+
+if __name__ == "__main__":
+    app.run(HOST, PORT)
